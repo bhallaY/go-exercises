948c6958ea895d666c1d9aad4b5168da01e37a2d
Yash Bhalla <bhalla.yash98@gmail.com>
2024-09-06 10:18:43 -0700

    Added docs

--- exercise.go
+++ exercise.go
@@ -1,5 +1,8 @@
 package main
 
+// Given two integers, adds them together
+//
+// [Addition Explanation]: https://www.mathsisfun.com/numbers/addition.html
 func Add(n1, n2 int) int {
 	return n1 + n2
 }
