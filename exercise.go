f96e1f319ea2b3b564824032f878073f9446be52
Yash Bhalla <bhalla.yash98@gmail.com>
2024-09-06 10:50:42 -0700

    Made function generic

--- exercise.go
+++ exercise.go
@@ -1,9 +1,17 @@
 package main
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Given two integers, adds them together
 //
 // [Addition Explanation]: https://www.mathsisfun.com/numbers/addition.html
-func Add(n1, n2 int) int {
+func Add[T Number](n1, n2 T) T {
 	return n1 + n2
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module exercises
 
 go 1.22.0
+
+require golang.org/x/exp v0.0.0-20240904232852-e7e105dedf7e
